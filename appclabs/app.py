6487170037ee43636a6e8ef85c5515be6a53b3f7
irvispano <52827417+irvispano@users.noreply.github.com>
2020-06-07 21:39:39 +0200

    prefinal draft

---
+++ appclabs/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify,Response
+import jwt
+import datetime
+import string
+import json
+import secrets
+import os
+app = Flask(__name__)
+sskey=os.getenv("SSKEY","secret")
+def generatejwttoken(username):
+    alphabet = string.ascii_letters + string.digits
+    jti = ''.join(secrets.choice(alphabet) for i in range(30,90))
+   
+    try:
+        if username=="test":
+            
+            payload = {
+                'iat': datetime.datetime.utcnow(),
+                'jti': jti,
+                'user': "username",
+                'date': str(datetime.date.today())
+            }
+        else:
+            print("222222")
+            payload = {
+                'iat': datetime.datetime.utcnow(),
+                'jti': jti,
+                'user': username,
+                'date': str(datetime.date.today())
+            }
+        token = jwt.encode(payload, sskey, algorithm='HS512')
+        print("brendatoken",token)
+        return token
+    except Exception:
+        print(Exception)
+        return Exception
+
+
+@app.route('/', methods=['POST'])
+def generateroute():
+    reqjson = request.get_data()
+    username=json.loads(reqjson)["username"]
+    try:
+        if username:
+            token = generatejwttoken(username)
+            
+        else:
+            token=generatejwttoken()
+            
+        resp = Response("Success")
+        resp.headers['`x-my-jwt`'] = token
+        return resp
+        
+      
+    except Exception as e:
+        print("eeeee",e)
+        return jsonify({
+            'status': 'Failure',
+            
+        })
+
+if __name__ == '__main__':
+    app.run(debug=True,port=80,host='0.0.0.0')
